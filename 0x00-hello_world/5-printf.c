4eb2a9b7cc10580ac79f624393d5887a17c76c2f
Opeejay <peterodinka@yahoo.com>
2023-01-18 14:04:08 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - main program block
  * Return: 0
  */
+
 int main(void)
+
 {
+
 printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
