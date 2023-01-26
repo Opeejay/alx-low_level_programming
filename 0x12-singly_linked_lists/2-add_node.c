e68defb9f1c2be23d476da6f1ac66a30d94b3d8b
Opeejay <peterodinka@yahoo.com>
2023-01-26 14:08:10 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,18 +12,15 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new, *temp;
 size_t nchar;
-
 new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
 new->str = strdup(str);
-
 for (nchar = 0; str[nchar]; nchar++)
 ;
 new->len = nchar;
 new->next = NULL;
 temp = *head;
-
 if (temp == NULL)
 {
 *head = new;
@@ -34,6 +31,5 @@ while (temp->next != NULL)
 temp = temp->next;
 temp->next = new;
 }
-
 return (*head);
 }
